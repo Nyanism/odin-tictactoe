961c2371b2119e772d5e50059157201eb47391f1
Nyanism <ojjstuff@gmail.com>
2022-09-21 16:11:34 +0800

    Implement displayBoard function

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const gameBoard = (() => {
-  const board = [[X,O,O],[X,O,O],[X,O,O]];
+  const board = [["X","O","O"],["X","O","O"],["X","O","O"]];
   return {board};
 })();
 
 const displayController = (() => {
+  
+  const displayBoard = (board) => {
+    for(let ii=0; ii<3; ii++) {
+      let row = document.querySelector(".row-one");
+      switch(ii) {
+        case 1:
+          row = document.querySelector(".row-two");
+          break;  
 
+        case 2:
+          row = document.querySelector(".row-three");
+          break;
+      }
+
+      for(let jj=0; jj<3; jj++){
+        let cell = document.createElement("span");
+        cell.textContent = board[ii][jj];
+        row.appendChild(cell);
+      }
+    }
+  };
+
+  return {displayBoard};
 })();
 
 const player = (name) => {
   return {name};
 };
+
+displayController.displayBoard(gameBoard.board);
